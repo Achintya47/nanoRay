a28a9fdde0f8021e56660b9a4758cb9f4e9055bf
Achintya Sharma <sharmaachintya49@gmail.com>
2026-02-09 17:57:44 +0000

    Add -> Sphere rendering and intersection

--- main.cpp
+++ main.cpp
@@ -5,9 +5,29 @@
 
 #include <iostream>
 
-color ray_color(const ray& r) {
-    return color(0, 0, 0);
+// hit_sphere using 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    double a = dot(r.direction(), r.direction());
+    double b = -2.0 * dot(r.direction(), oc);
+    double c = dot(oc, oc) - radius * radius;
+    double discriminant = b * b - 4 * a * c;
+
+    return(discriminant >= 0);
 }
+
+color ray_color(const ray& r) {
+    
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+    
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    // A lerp bend of White and Blue-
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
+
 
 int main() {
 
