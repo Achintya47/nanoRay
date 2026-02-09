0ca05ebc57598b2fa5f974b110bc6f56656853d5
Achintya Sharma <sharmaachintya49@gmail.com>
2026-02-09 18:55:20 +0000

    Fix -> Calculation error

--- main.cpp
+++ main.cpp
@@ -12,21 +12,19 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
     double b = -2.0 * dot(r.direction(), oc);
     double c = dot(oc, oc) - radius * radius;
     double discriminant = b * b - 4 * a * c;
-
     return(discriminant >= 0);
 }
 
 color ray_color(const ray& r) {
-    
+
     if (hit_sphere(point3(0, 0, -1), 0.5, r))
         return color(1, 0, 0);
     
     vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
+    double a = 0.5*(unit_direction.y() + 1.0);
     // A lerp bend of White and Blue-
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
-
 
 
 int main() {
@@ -60,7 +58,9 @@ int main() {
     // |           |
     // |           |
     //  -----------
-    vec3 viewport_u = vec3(viewport_height, 0, 0);
+
+    // Calculation error, viewport_height -> viewport_width
+    vec3 viewport_u = vec3(viewport_width, 0, 0);
     vec3 viewport_v = vec3(0, -viewport_height, 0);
 
     // Horizontal and vertical delta vectors from pixel to pixel
@@ -73,7 +73,7 @@ int main() {
 
     // RENDER
        
-    std::cout << "P3\n" << image_height << " " << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for(int j = 0; j < image_height; j++){
         std::clog << "\rScanLines remaining: " << (image_height - j) << " " << std::flush;
